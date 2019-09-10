65a4b9b8a90f0133120b8d8064e5d6eba09a2876
FadlyJaya <jaya.fadly@gmail.com>
2019-09-10 07:46:30 -0700

    Initial Commite

--- lib/home.dart
+++ lib/home.dart
@@ -6,35 +6,40 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       home: Scaffold(
-        appBar: new AppBar(
-          title: const Text('Food Recipes'),
+        appBar: AppBar(
+          title: Text('Food Recipes'),
         ),
-        body: FoodRecipesView(),
+        body: foodRecipesView(),
       ),
     );
   }
 
-  GridView FoodRecipesView() => GridView.builder(
+  GridView foodRecipesView() => GridView.builder(
       itemCount: foodName.length,
-      gridDelegate:
-          new SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
       itemBuilder: (BuildContext context, index) => Center(
-            child: ListTile(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (BuildContext context) => DetailScreen(
-                              FoodName: foodName[index],
-                              FoodImage: foodImage[index],
-                              FoodIngredients: foodIngredients[index],
-                            )));
-              },
-              title: Column(
-                children: <Widget>[
-                  Image.network(foodImage[index], width: 380, height: 130, fit: BoxFit.cover,),
-                  Text(foodName[index])
-                ],
+            child: Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+              child: ListTile(
+                title: Column(
+                  children: <Widget>[
+                    Padding(padding: EdgeInsets.all(5.0)),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(10.0),
+                      child: Image.network(foodImage[index], width: 300, height: 100, fit: BoxFit.cover,),
+                    ),
+                    SizedBox(height: 10),
+                    Text(foodName[index], style: TextStyle(color: Colors.blueGrey, fontSize: 13),)
+                  ],
+                ),
+                onTap: (){
+                  Navigator.push(context, MaterialPageRoute(
+                      builder: (BuildContext context) => DetailScreen(
+                        foodImage: foodImage[index],
+                        foodName: foodName[index],
+                        foodIngredients: foodIngredients[index],
+                      )));
+                },
               ),
             ),
           ));
@@ -66,16 +71,16 @@ class HomePage extends StatelessWidget {
   ];
 
   List<String> foodIngredients = [
-    '=> 1/2 kg sayap ayam\n=> 1 sachet tepung bumbu sasa',
-    '=> 1 ekor ayam, potong sesuai selera\n=> 2 batang serai, ambil bagian putihnya, memarkan\n=> 4 lembar daun salam\n=> 20 gr asam jawa, larutkan dan remas-remas dalam 3sdm air panas, ambil airnya\n=> 500 ml air kelapa',
-    '=> 1 ekor ayam pejantan\n=> 3 Liter Air\n=> 4 butir kemiri dihaluskan\n=> 3 siung bawang putih, geprek\n=> 3 ruas lengkuas\n=> 3 lembar daun jeruk\n=> 1 sdm ketumbar bubuk\n=> Garam, secukupnya',
-    '=> 1/2 potong dada ayam, potong kecil\n=> 2 bks tepung serba guna\n=> 4 siung bawang putih, haluskan\n=> 1 bks saus tiram\n=> Saus tomat, secukupnya\n=> Saus sambal, secukupnya\n=> Merica, secukupnya\n=> Kaldu bubuk Alsultan Non MSG Ayam, secukupnya\n=> Air, secukupnya\n=> Minyak untuk menggoreng',
-    '=> 2 liter air\n=> 300 gr daging sapi sandung lamur, portong-potong\n=> 2 cm jahe, memarkan\n=> 3 sdm minyak untuk menumis\n=> 2 batang serai, memarkan\n=> 2 cm lengkuas, memarkan\n=> 50 gr lobak, iris tipis\n=> 1 batang seledri, cincang halus\n=> 1 batang daun bacang, iris\n=> 1 sdt gula pasir\n=> Garam secukupnya\n=> 1 sdm bawang merah goreng\n=> 50 gr kacang kedelai, goreng',
-    '=> 300 gr tahu puti\n=> 1 sdt kaldu ayam (bisa pakai Alsultan)\n=> 1/2 sdt gula pasir\n=> 1/4 sdt merica bubuk\n=> 1/4 sdt bawang putih bubuk\n=> 1 butir putih telur',
-    '=> 100 gr ayam giling\n=> 10 butir telur puyuh, rebus\n=> 1 buah tahu putih\n=> 1 bks mie telur\n=> 1 butir telur\n=> 2 siung bawang putih, haluskan\n=> 1/2 sdt lada bubuk\n=> 1/2 sdt garam\n=> Minyak, untuk menggoreng',
-    '=> 400g daging sapi giling\n=> ½ btr bawang bombay\n=> 1btr telur\n=> ¼ sdt pala bubuk\n=> 50g tepung roti\n=> 3 sdm mentega\n=> 1 bks Masako Sapi (7,5 g) (optional)',
-    '=> Salmon segar, jika masih ada kulitnya tidak apa.\n=> 1 cup cream masak\n=> 100 cc white wine (bisa diganti dengan air)\n=> 1 buah bawang merah, dipotong kecil- kecil\n=> Daun parseley, diiris halus\n=> Mentega secukupnya untuk menggoreng salmon\n=> Garam\n=> Lada hitam\n=> ½ buah buah lemon segar, ambil airnya',
-    '=> 1 mangkok nasi\n=> 1 butir telur\n=> Keju parut\n=> Garam secukupnya\n=> Lada sedikit\n=> Keju cheddar, potong kotak (untuk isian)\n=> Tepung panir',
+    '1/2 kg sayap ayam\n1 sachet tepung bumbu sasa',
+    '1 ekor ayam, potong sesuai selera\n2 batang serai, ambil bagian putihnya, memarkan\n4 lembar daun salam\n20 gr asam jawa, larutkan dan remas-remas dalam 3 sdm air panas, ambil airnya\n500 ml air kelapa',
+    '1 ekor ayam pejantan\n3 Liter Air\n4 butir kemiri dihaluskan\n3 siung bawang putih, geprek\n3 ruas lengkuas\n3 lembar daun jeruk\n1 sdm ketumbar bubuk\nGaram, secukupnya',
+    '1/2 potong dada ayam, potong kecil\n2 bks tepung serba guna\n4 siung bawang putih, haluskan\n1 bks saus tiram\nSaus tomat, secukupnya\nSaus sambal, secukupnya\nMerica, secukupnya\nKaldu bubuk Alsultan Non MSG Ayam, secukupnya\nAir, secukupnya\nMinyak untuk menggoreng',
+    '2 liter air\n300 gr daging sapi sandung lamur, portong-potong\n2 cm jahe, memarkan\n3 sdm minyak untuk menumis\n2 batang serai, memarkan\n2 cm lengkuas, memarkan\n50 gr lobak, iris tipis\n1 batang seledri, cincang halus\n1 batang daun bacang, iris\n1 sdt gula pasir\nGaram secukupnya\n1 sdm bawang merah goreng\n50 gr kacang kedelai, goreng',
+    '300 gr tahu puti\n1 sdt kaldu ayam (bisa pakai Alsultan)\n1/2 sdt gula pasir\n1/4 sdt merica bubuk\n1/4 sdt bawang putih bubuk\n1 butir putih telur',
+    '100 gr ayam giling\n10 butir telur puyuh, rebus\n1 buah tahu putih\n1 bks mie telur\n1 butir telur\n2 siung bawang putih, haluskan\n1/2 sdt lada bubuk\n1/2 sdt garam\nMinyak, untuk menggoreng',
+    '400g daging sapi giling\n½ btr bawang bombay\n1btr telur\n¼ sdt pala bubuk\n50g tepung roti\n3 sdm mentega\n1 bks Masako Sapi (7,5 g) (optional)',
+    'Salmon segar, jika masih ada kulitnya tidak apa.\n1 cup cream masak\n100 cc white wine (bisa diganti dengan air)\n1 buah bawang merah, dipotong kecil- kecil\nDaun parseley, diiris halus\nMentega secukupnya untuk menggoreng salmon\nGaram\nLada hitam\n½ buah buah lemon segar, ambil airnya',
+    '1 mangkok nasi\n1 butir telur\nKeju parut\nGaram secukupnya\nLada sedikit\nKeju cheddar, potong kotak (untuk isian)\nTepung panir',
   ];
 
 //Source : https://resepkoki.co/resep-masakan/
